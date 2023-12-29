a9f8c665b69bc564a7a65f28c26e22f1c22d2edd
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-12-29 14:30:24 -0300

    commit

---
+++ src/components/pages/AuthPage.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const AuthPage = (props) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target[0];
+    axios
+      .post(import.meta.env.VITE_CHAT_POST_URL, { username: value })
+      .then((r) => props.onAuth({ ...r.data, secret: value }))
+      .catch((e) => console.log("Auth Error", e));
+  };
+
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Bem-Vindo(a) 👋</div>
+
+        <div className="form-subtitle">Insira seu usuário e comece a conversar</div>
+
+        <div className="auth">
+          <div className="auth-label">Usuário</div>
+          <input className="auth-input" name="username" />
+          <button className="auth-button" type="submit">
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
